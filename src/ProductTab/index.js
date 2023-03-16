400de7c8bcb4de4e98881594448b0448529ea3a2
hnhnsko12 <127993875+hnhnsko12@users.noreply.github.com>
2023-03-16 02:18:31 +0100

    Delete src/ProductTab directory

--- src/ProductTab/index.js
+++
@@ -1,6 +0,0 @@
-import ProductTab from "./ProductTab";
-import ProductTabTwo from "./ProductTabTwo";
-import ProductTabThree from "./ProductTabThree";
-import ProductTabFour from "./ProductTabFour";
-
-export { ProductTab, ProductTabTwo, ProductTabThree, ProductTabFour };
